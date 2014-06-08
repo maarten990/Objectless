1625d79409e0953965fc3d00921c9565596e7dcb
Maarten de Jonge <mdejonge1990@gmail.com>
2014-06-09 01:31:05 +0200

    add mapping from Component names to type indices

--- Engine/ComponentManager.h
+++ Engine/ComponentManager.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <typeinfo>
 #include <typeindex>
+#include <string>
 #include "Component.h"
 #include "System.h"
 
@@ -17,18 +18,28 @@ public:
 	ComponentManager();
 	~ComponentManager();
 
-	/* Creates a factory function for the supplied component id. Has to be
-	 * called once for every type of component. */
+	/* Creates a factory function for the supplied component id, and enters the
+	 * component in the map of type_info string. Has to be called once for every
+	 * type of component. */
 	template <typename T>
 	void register_component() {
 		_constructors[T::id()] = []() { return new T(); };
+		_type_indices.insert(make_pair(typeid(T).name(), T::id()));
 	}
 
 	/* Create a component of the given type, belonging to a given entity. */
 	Component *construct(type_index type);
-	
+
+	/* Return a pointer to the type_index corresponding to its string
+	 * representation. Returns NULL for an unknown/invalid name. */
+	type_index *index_from_name(string name);
+
 private:
 	map<type_index, function<Component*()> > _constructors;
+
+	/* a mapping of type_info strings to a corresponding type_index object */
+	map<string, type_index> _type_indices;
+	
 };
 
 #endif
--- Engine/src/ComponentManager.cpp
+++ Engine/src/ComponentManager.cpp
@@ -1,6 +1,9 @@
+#include <string>
 #include "ComponentManager.h"
 #include "Component.h"
 #include "System.h"
+
+using namespace std;
 
 ComponentManager::ComponentManager()
 {
@@ -15,4 +18,14 @@ Component *ComponentManager::construct(type_index type)
 	Component *component = _constructors[type]();
 
 	return component;
+}
+
+type_index *ComponentManager::index_from_name(string name)
+{
+	map<string, type_index>::iterator it = _type_indices.find(name);
+
+	if (it != _type_indices.end())
+		return &it->second;
+	else
+		return NULL;
 }
