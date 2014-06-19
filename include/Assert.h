51c46c651fcb6b3ce5a3f66ce5896aa0fc9c80af
ivarboms <ivarboms@gmail.com>
2014-06-19 22:55:24 +0200

    Add assert macro.

---
+++ include/Assert.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdio.h>
+
+#ifdef _MSC_VER
+#define DEBUG_BREAK __debugbreak()
+#define FUNCTION_SIGNATURE __FUNCSIG__
+#else
+#include <signal.h>
+#define DEBUG_BREAK raise(SIGTRAP)
+#define FUNCTION_SIGNATURE __PRETTY_FUNCTION__
+#endif
+
+
+#if _DEBUG
+#define assert2(condition, format, ...) \
+do \
+{ \
+	if (!(condition)) \
+	{ \
+		printf("Assertion failed: %s\n", #condition); \
+		printf((format), __VA_ARGS__); \
+		printf("\nFile: %s:%i\nFunction: %s\n", __FILE__, __LINE__, FUNCTION_SIGNATURE); \
+		DEBUG_BREAK; \
+	} \
+} while (false)
+
+#else
+//The void casting prevents warnings about unused variables.
+#define assert2(condition, format, ...) ((void)(condition), (void)(format))
+#endif
