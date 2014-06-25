9a83a30db9c2f3b31b7b6239697ce522fbdff78e
ivarboms <ivarboms@gmail.com>
2014-06-25 22:53:30 +0200

    Add GeometryDrawer class.

---
+++ include/GeometryDrawer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Engine/System.h"
+#include <vector>
+#include <stdint.h>
+
+struct SDL_Renderer;
+class GraphicsSystem;
+
+class GeometryDrawer : public System
+{
+	struct LineSegment
+	{
+		float start_x, end_x;
+		float start_y, end_y;
+
+		float remaining_lifetime;
+
+		uint32_t color_rgba;
+	};
+
+
+public:
+	GeometryDrawer(SDL_Renderer* renderer, GraphicsSystem* graphics_system);
+	~GeometryDrawer();
+
+	virtual void step(float delta_time, EntityManager* em) override;
+
+	void draw_line(float start_x, float start_y, float end_x, float end_y,
+		uint32_t color_rgba = 0xFFFFFFFF, float duration_seconds = 0.0f);
+
+private:
+	void drawGeometry();
+
+	uint64_t _graphics_callback_id;
+	GraphicsSystem* _graphics_system;
+	SDL_Renderer* _renderer;
+	std::vector<LineSegment> _lines;
+};
---
+++ src/GeometryDrawer.cpp
@@ -0,0 +1,62 @@
+#include "GeometryDrawer.h"
+
+#include <algorithm>
+#include <SDL_render.h>
+
+#include "External/SDL2_gfx/SDL2_gfxPrimitives.h"
+#include "Engine/EntityManager.h"
+#include "Engine/Component.h"
+#include "Components.h"
+#include "GraphicsSystem.h"
+
+
+GeometryDrawer::GeometryDrawer(SDL_Renderer* renderer, GraphicsSystem* graphics_system)
+	: _renderer(renderer)
+	, _graphics_system(graphics_system)
+{
+	_graphics_callback_id = _graphics_system->registerPostRenderCallback(
+		std::bind(&GeometryDrawer::drawGeometry, this));
+}
+
+GeometryDrawer::~GeometryDrawer()
+{
+	_graphics_system->unregisterPostRenderCallback(_graphics_callback_id);
+}
+
+void GeometryDrawer::step(float delta_time, EntityManager*)
+{
+	if (_lines.empty()) {
+		return;
+	}
+	for (LineSegment& line : _lines) {
+		line.remaining_lifetime -= delta_time;
+	}
+
+	//Remove line segments whose lifetime has expired.
+	_lines.erase(std::remove_if(std::begin(_lines), std::end(_lines),
+		[](const LineSegment& line) {return line.remaining_lifetime <= 0.0f; }));
+}
+
+void GeometryDrawer::draw_line(float start_x, float start_y, float end_x, float end_y,
+	uint32_t color_rgba /*= 0xFFFFFFFF*/, float duration_seconds /*= 0.0f*/)
+{
+	LineSegment line{ start_x, end_x, start_y, end_y, duration_seconds, color_rgba, };
+	line.color_rgba = color_rgba;
+	_lines.push_back(line);
+}
+
+uint32_t rgbaToAgbr(uint32_t v)
+{
+	return ((v & 0xFF) << 24) | ((v & 0xFF00) << 8) | ((v & 0xFF0000) >> 8)
+		| ((v & 0xFF000000) >> 24);
+}
+
+void GeometryDrawer::drawGeometry()
+{
+	for (const LineSegment& line : _lines) {
+		//TODO figure out why this conversion is needed. According to the docs, SDL uses RGBA too.
+		uint32_t color = rgbaToAgbr(line.color_rgba);
+		aalineColor(_renderer, (int16_t)line.start_x, (int16_t)line.start_y, (int16_t)line.end_x,
+			(int16_t)line.end_y, color);
+	}
+}
