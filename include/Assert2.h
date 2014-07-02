b42a08404f8fc53b3f9b23368ccae19814a5bbd3
ivarboms <ivarboms@gmail.com>
2014-07-02 04:03:13 +0200

    Assert2.h: Replace VS-specific pragmas with macros.

--- include/Assert2.h
+++ include/Assert2.h
@@ -5,10 +5,16 @@
 #ifdef _MSC_VER
 #define DEBUG_BREAK __debugbreak()
 #define FUNCTION_SIGNATURE __FUNCSIG__
+#define MSC_PRAGMA_PUSH __pragma(warning(push))
+#define MSC_PRAGMA_POP __pragma(warning(pop))
+#define MSC_PRAGMA_WARNING_DISABLE(text) __pragma(warning(text))
 #else
 #include <signal.h>
 #define DEBUG_BREAK raise(SIGTRAP)
 #define FUNCTION_SIGNATURE __PRETTY_FUNCTION__
+#define MSC_PRAGMA_PUSH
+#define MSC_PRAGMA_POP
+#define MSC_PRAGMA_WARNING_DISABLE(text)
 #endif
 
 
@@ -16,9 +22,9 @@
 #define assert2(condition, ...) \
 do \
 { \
-__pragma(warning(push)) \
-__pragma(warning(disable:4127)) \
-if (!(condition)) \
+MSC_PRAGMA_PUSH \
+MSC_PRAGMA_WARNING_DISABLE(disable:4127) \
+	if (!(condition)) \
 	{ \
 		printf("Assertion failed: %s\n", #condition); \
 		printf(__VA_ARGS__); \
@@ -26,7 +32,7 @@ if (!(condition)) \
 		DEBUG_BREAK; \
 	} \
 } while (false) \
-__pragma(warning(pop))
+MSC_PRAGMA_POP
 
 #else
 //The void casting prevents warnings about unused variables.
