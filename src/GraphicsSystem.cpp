1a0ddc8c30c30aaa146f60387341cca37094339f
ivarboms <ivarboms@gmail.com>
2014-07-23 15:42:01 +0200

    GraphicsSystem: Now uses the Transform components' rotation and scale when rendering.

--- src/GraphicsSystem.cpp
+++ src/GraphicsSystem.cpp
@@ -5,6 +5,7 @@
 #include "GraphicsSystem.h"
 #include "Components.h"
 #include "Engine/EntityManager.h"
+#include "Engine/Graphics/Texture.h"
 #include "Assert2.h"
 
 GraphicsSystem::GraphicsSystem(EntityManager *em, int window_width, int window_height)
@@ -52,23 +53,31 @@ void GraphicsSystem::step(float /*delta_time*/)
 
 	for (unsigned int id : _entities) {
 		Graphics *g = _entitymanager->get_component<Graphics>(id);
-		Transform *p = _entitymanager->get_component<Transform>(id);
 		if (!g->is_visible) {
 			continue;
 		}
 
+		Transform *transform = _entitymanager->get_component<Transform>(id);
+
 		/* create destination rect using the desired position
 		 * and the texture's dimensions */
 		SDL_Rect dest;
-		dest.x = (int)p->x;
-		dest.y = (int)p->y;
-		dest.w = g->width;
-		dest.h = g->height;
+		dest.x = (int)transform->position.x;
+		dest.y = (int)transform->position.y;
+		dest.w = (int)(g->texture->getWidth() * transform->scale.x);
+		dest.h = (int)(g->texture->getHeight() * transform->scale.y);
 
-		SDL_RenderCopy(_renderer, g->texture, nullptr, &dest);
+		const float radians_to_degrees = 57.324841f;
+		const float rotation_degrees = transform->rotation.GetAngle() * radians_to_degrees;
+		SDL_Point zero{ 0, 0 };
+		bool success = SDL_RenderCopyEx(_renderer, g->texture->getTexture(), nullptr,
+			&dest, rotation_degrees, &zero, SDL_FLIP_NONE) == 0;
+
+		assert2(success, "Failed to render texture '%s'. SDL error: ",
+			g->texture->getName().c_str(), SDL_GetError());
 	}
 
-	for (auto post_render_callback : _post_render_callbacks)
+	for (auto& post_render_callback : _post_render_callbacks)
 	{
 		post_render_callback.function();
 	}
