983d8e6b837ac70c1b7cb7f6dcffc39919e07a81
ivarboms <ivarboms@gmail.com>
2014-07-24 17:53:41 +0200

    MovementSystem: Affect rigid body's linear velocity instead of directly changing the Transform.

--- src/MovementSystem.cpp
+++ src/MovementSystem.cpp
@@ -1,20 +1,34 @@
 #include "MovementSystem.h"
 #include "Components.h"
 #include "Engine/EntityManager.h"
-#include <stdio.h>
+#include "Engine/Physics/PhysicsBodyComponent.h"
+#include <Box2D/Dynamics/b2Body.h>
 
 MovementSystem::MovementSystem(EntityManager *em)
 	: _em(em)
 {
 }
 
-void MovementSystem::step(float dt)
+void MovementSystem::step(float /*dt*/)
 {
 	for (unsigned int id: _entities) {
 		Movement *mv = _em->get_component<Movement>(id);
-		Transform *t = _em->get_component<Transform>(id);
+		PhysicsBodyComponent* rb = _em->get_component<PhysicsBodyComponent>(id);
 
-		t->x += mv->x_speed * mv->x_multiplier;
-		t->y += mv->y_speed * mv->y_multiplier;
+		//todo remove this check, will be redundant when System::notify_created
+		//works correctly.
+		if (mv == nullptr || rb == nullptr)
+		{
+			return;
+		}
+		b2Vec2 linear_velocity(mv->speed);
+		linear_velocity.x *= mv->speed_multiplier.x;
+		linear_velocity.y *= mv->speed_multiplier.y;
+
+		//todo don't do this, using a more sensible world scale should remove the
+		//need for this.
+		linear_velocity *= 10000.0f;
+
+		rb->body->SetLinearVelocity(linear_velocity);
 	}
 }
